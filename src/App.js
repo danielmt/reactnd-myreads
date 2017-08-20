a70c25ff9a255908e6040dd680bbb25634ceb14e
Daniel Mascarenhas <daniel.mts@gmail.com>
2017-08-20 20:34:03 -0300

    feat: Getting the list of books from the API
    App now gets the list of books from the API and splits in three lists on its
    state: currentlyReading, wantToRead and read.
    
    Book and BookShelf were extracted as components from ListBooks.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
-// import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
+  state = {
+    currentlyReading: [],
+    wantToRead: [],
+    read: [],
+  };
+
+  componentDidMount = () => {
+    BooksAPI.getAll().then(books => {
+      let currentlyReading = [],
+          wantToRead = [],
+          read = [];
+
+      books.map(book => {
+        switch(book.shelf) {
+        case 'currentlyReading':
+          currentlyReading.push(book);
+          break;
+        case 'wantToRead':
+          wantToRead.push(book);
+          break;
+        case 'read':
+          read.push(book);
+          break;
+        default:
+          console.log('Unsupported book shelf: ', book.shelf)
+        }
+      });
+
+      this.setState({ currentlyReading, wantToRead, read });
+    });
+  };
+
   render() {
     return (
       <div className="app">
+        <Route exact path="/" render={() => (
+          <ListBooks currentlyReading={ this.state.currentlyReading }
+                     wantToRead={ this.state.wantToRead }
+                     read={ this.state.read }
+                   />
+          )}/>
         <Route path="/search" render={SearchBooks}/>
-        <Route exact path="/" render={ListBooks}/>
       </div>
     )
   }
